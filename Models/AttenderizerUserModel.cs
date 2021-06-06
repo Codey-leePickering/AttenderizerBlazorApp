b01dd56795b40dcc1de3432788da8fda83d63a58
Codey-lee Pickering <30005274@student.toiohomai.ac.nz>
2021-06-06 20:45:38 +1200

    Add project files.

---
+++ Models/AttenderizerUserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttenderizerBlazorApp.Models
+{
+    public class Rootobject
+    {
+        public AttenderizerUserModel[] Property1 { get; set; }
+    }
+
+    public class AttenderizerUserModel
+    {
+        public string id { get; set; }
+        public int username { get; set; }
+        public string password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public bool isAbsent { get; set; }
+    }
+
+}
