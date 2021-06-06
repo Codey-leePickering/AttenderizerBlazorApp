15f5e3832828f5da0f146c8db32bf95520549371
NoxiCola <karl.perez94@gmail.com>
2021-06-07 06:10:04 +1200

    Editted QR Model
    Added
    Reset Attendance Button
    Refresh List Button
    
    Enabled QR code to store to the database

---
+++ Models/QRModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttenderizerBlazorApp.Models
+{
+    public class QRModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("qRCode")]
+        public string QrCode { get; set; }
+    }
+}
---
+++ Services/InsertService.cs
@@ -0,0 +1,17 @@
+﻿using AttenderizerBlazorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AttenderizerBlazorApp.Services
+{
+    public class InsertService
+    {
+     
+    }
+
+    
+}
